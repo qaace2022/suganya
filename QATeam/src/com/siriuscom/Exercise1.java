1686b64c183eef9a2d95fe58489b1fa156b74352
suganyakandasamy <99638400+suganyakandasamy@users.noreply.github.com>
2022-02-14 17:47:30 +0530

    Create Exercise1.java
    initial commit

---
+++ QATeam/src/com/siriuscom/Exercise1.java
@@ -0,0 +1,15 @@
+package com.siriuscom;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		int a=10;
+	    int b=15;
+	    int c=a*b;
+	    System.out.println("Product of "+ a + " and "+ b +" is "+ c);
+
+		
+
+	}
+
+}
