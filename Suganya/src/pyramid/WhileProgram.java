bc1a36e243d6373547adebffe34e4fb3b5bb7601
suganyakandasamy <99638400+suganyakandasamy@users.noreply.github.com>
2022-02-15 16:59:51 +0530

    Create WhileProgram.java
    while loop

---
+++ Suganya/src/pyramid/WhileProgram.java
@@ -0,0 +1,15 @@
+package pyramid;
+
+public class WhileProgram {
+
+	public static void main(String[] args) {
+	int i=1;
+			while(i<6)
+			{
+				System.out.println("i will get printed once for sure!");
+				i++;
+			}
+
+	}
+
+}
